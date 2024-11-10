9610bbc91c9793c811f9631a768233ad3d0799cb
bloodofspring <t.shagin@gmail.com>
2024-11-10 21:21:54 +0300

    feat: Модифицирован метод  nearest_missions

--- client_handlers/start_cmd.py
+++ client_handlers/start_cmd.py
@@ -1,5 +1,5 @@
 from client_handlers.base import *
-from datetime import datetime
+from datetime import datetime, time
 
 from database.models import SendTime
 
@@ -10,14 +10,15 @@ class StartCmd(BaseHandler):
     @property
     def nearest_missions(self):
         now = datetime.now()
-
         nearest = SendTime.select().where(
             ((SendTime.consider_date & SendTime.send_date == now.date()) |
             (SendTime.weekday.between(0, 6) & SendTime.weekday == now.weekday())) &
             (SendTime.send_time >= now.time())
         ).order_by(SendTime.send_time)
+        max_ = max(map(lambda t: t.send_time, nearest))
+        same_max_operations = tuple(filter(lambda t: t.send_time == max_, nearest))
 
-        return map(lambda t: t.oper[0], nearest)
+        return map(lambda t: t.oper[0], same_max_operations)
 
     async def func(self):
-        await self.request.
+        await self.request.answer("Бот запущен!\nБлижайшая операция: {}")
