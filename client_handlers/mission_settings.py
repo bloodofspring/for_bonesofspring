8a74e575281ffe640854e73b89eb8663e39b901c
bloodofspring <t.shagin@gmail.com>
2024-11-11 12:20:45 +0300

    feat: Шаблонизированы обработчики для миссий

--- client_handlers/mission_settings.py
+++ client_handlers/mission_settings.py
@@ -1,9 +1,22 @@
 from client_handlers.base import *
 
 
+class MissionsList(BaseHandler):
+    FILTER = regex("Мои напоминания")
+
+    async def func(self):
+        pass
+
+
 class AddMission(BaseHandler):
-    pass
+    FILTER = regex("Добавить напоминание")
+
+    async def func(self):
+        pass
 
 
 class RmMission(BaseHandler):
-    pass
+    FILTER = create(lambda _, __, q: q and q.data and q.data.startswith("rm_mission"))
+
+    async def func(self):
+        pass
